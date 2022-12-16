b1ca35a5267f74dba1a77476cf70d01b5c346372
SAI-SRINIVASA-SUBRAMANYAM <subramanyam.sista@gmail.com>
2022-12-16 07:31:28 +0530

    dockerfile added

--- sensor/config.py
+++ sensor/config.py
@@ -7,7 +7,7 @@ import os
 
 @dataclass
 class EnvironmentVariable:
-    mongo_db_url:str = os.getenv("MONGO_DB_URL")
+    mongo_db_url:str = os.getenv("MONGODB_URL")
     aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
     aws_access_secret_key:str = os.getenv("AWS_SECRET_ACCESS_KEY")
 
